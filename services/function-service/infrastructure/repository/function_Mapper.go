b5b5967c5f2cabd34673751ea788930523aa2170
Fadli <fadliarz03@gmail.com>
2025-07-30 22:02:40 +0700

    feat: update function-service

---
+++ services/function-service/infrastructure/repository/function_Mapper.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"fmt"
+
+	"github.com/fadliarz/distributed-faas/services/function-service/domain/domain-core"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type FunctionDataAccessMapperImpl struct{}
+
+func NewFunctionDataAccessMapper() FunctionDataAccessMapper {
+	return &FunctionDataAccessMapperImpl{}
+}
+
+func (m *FunctionDataAccessMapperImpl) Entity(function *domain.Function) (*FunctionEntity, error) {
+	if function == nil {
+		return nil, nil
+	}
+
+	functionID, err := primitive.ObjectIDFromHex(function.FunctionID.String())
+	if err != nil {
+		return nil, fmt.Errorf("invalid function ID: %w", err)
+	}
+
+	return &FunctionEntity{
+		FunctionID:    functionID,
+		UserID:        function.UserID.String(),
+		SourceCodeURL: function.SourceCodeURL.String(),
+	}, nil
+}
+
+func (m *FunctionDataAccessMapperImpl) Domain(entity *FunctionEntity) *domain.Function {
+	if entity == nil {
+		return nil
+	}
+
+	return &domain.Function{
+		FunctionID:    domain.NewFunctionID(entity.FunctionID.Hex()),
+		UserID:        domain.NewUserID(entity.UserID),
+		SourceCodeURL: domain.NewSourceCodeURL(entity.SourceCodeURL),
+	}
+}
