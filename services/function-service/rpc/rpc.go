3c12d1cc1edf1b84eb9974366477e151caa09fb9
Fadli <fadliarz03@gmail.com>
2025-07-31 06:39:09 +0700

    feat(function-service/rpc): implement get function upload presigned url request handler

--- services/function-service/rpc/rpc.go
+++ services/function-service/rpc/rpc.go
@@ -2,10 +2,14 @@ package rpc
 
 import (
 	"context"
+	"errors"
 
 	"github.com/fadliarz/distributed-faas/services/function-service/domain/application-service"
 	function_service_v1 "github.com/fadliarz/distributed-faas/services/function-service/gen/go/function-service/v1"
+	"github.com/fadliarz/distributed-faas/services/registrar-service/domain/domain-core"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type FunctionServer struct {
@@ -39,5 +43,27 @@ func (s *FunctionServer) CreateFunction(ctx context.Context, req *function_servi
 		FunctionId: functionID.String(),
 		Status:     "success",
 		Message:    "Function created successfully",
+	}, nil
+}
+
+func (s *FunctionServer) GetFunctionUploadPresignedURL(ctx context.Context, req *function_service_v1.GetFunctionUploadPresignedURLRequest) (*function_service_v1.GetFunctionUploadPresignedURLResponse, error) {
+	query := &application.GetFunctionUploadPresignedURLQuery{
+		UserID:     req.UserId,
+		FunctionID: req.FunctionId,
+	}
+
+	presignedURL, err := s.handler.GetFunctionUploadPresignedURL(ctx, query)
+	if err != nil {
+		if errors.Is(err, domain.ErrUserNotAuthorized) {
+			return nil, status.Errorf(codes.PermissionDenied, "user not authorized: %v", err)
+		}
+
+		return nil, status.Errorf(codes.Internal, "failed to get presigned URL: %v", err)
+	}
+
+	return &function_service_v1.GetFunctionUploadPresignedURLResponse{
+		PresignedUrl: presignedURL,
+		Status:       "success",
+		Message:      "Presigned URL retrieved successfully",
 	}, nil
 }
