e10e39e7b1e7e6f8bdad59a7c4394542fa8e1b81
Fadli <fadliarz03@gmail.com>
2025-08-12 08:47:06 +0700

    remove unused function

--- tests/integration/create-invocation/assertion_helper.go
+++ tests/integration/create-invocation/assertion_helper.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/fadliarz/distributed-faas/services/dispatcher-service/domain/application-service"
-	fs_repository "github.com/fadliarz/distributed-faas/services/function-service/infrastructure/repository"
 	is_repository "github.com/fadliarz/distributed-faas/services/invocation-service/infrastructure/repository"
 	m_repository "github.com/fadliarz/distributed-faas/services/machine/infrastructure/repository"
 	invocation_service_v1 "github.com/fadliarz/distributed-faas/tests/integration/gen/go/invocation-service/v1"
@@ -46,21 +45,6 @@ func (ah *AssertionHelper) AssertInvocationUnauthorized(createInvocationRespose 
 
 	require.True(ah.t, ok, "Error should be a gRPC status error")
 	require.Equal(ah.t, codes.PermissionDenied.String(), st.Code().String(), "Expected PERMISSION_DENIED status code")
-}
-
-func (ah *AssertionHelper) AssertFunctionPersistedInFunctionMongoDB(ctx context.Context, client *mongo.Client, functionID string) {
-	collection := client.Database(ah.config.MongoConfig.FunctionDatabase).Collection(ah.config.MongoConfig.FunctionCollection)
-
-	objectID, err := primitive.ObjectIDFromHex(functionID)
-	require.NoError(ah.t, err, "Failed to convert Function ID to ObjectID")
-
-	var function fs_repository.FunctionEntity
-	err = collection.FindOne(ctx, bson.M{"_id": objectID}).Decode(&function)
-	require.NoError(ah.t, err, "Failed to find function in MongoDB")
-
-	require.NotEmpty(ah.t, function.FunctionID, "Function ID should not be empty")
-	require.NotEmpty(ah.t, function.UserID, "User ID should not be empty")
-	require.Empty(ah.t, function.SourceCodeURL, "")
 }
 
 func (ah *AssertionHelper) AssertInvocationPersistedInMongoDB(ctx context.Context, client *mongo.Client, invocationID string) {
